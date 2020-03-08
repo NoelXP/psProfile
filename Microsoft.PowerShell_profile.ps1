c05dfc374fc83c4b6c6a09ac2491b4a57d0ab6d8
Noel <NoelXP@GMail.com>
2020-03-08 11:50:54 -0400

    Removed useless choco @ 1150

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -135,9 +135,3 @@ function prompt
 	" "
 } 
 
-
-# Chocolatey profile
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
